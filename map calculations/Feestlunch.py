88242a354d40a42c0f29a6b8bfdd6e11cd5e9d2e
rouamu <Roua.mulham@gmail.com>
2021-09-10 13:26:21 +0200

    calculatie

---
+++ map calculations/Feestlunch.py
@@ -0,0 +1,12 @@
+croissantjes = (17)
+prijs = (0.39)
+stokbroden = (2)
+Prijs = (2.78)
+kortingsbonnen = (1.50)
+totaal = (croissantjes * prijs + stokbroden * Prijs - kortingsbonnen)
+
+print ("Feestlunch kosten" + str(totaal))
+
+
+
+
---
+++ map calculations/Speelhaldag.py
@@ -0,0 +1,8 @@
+toegangsticket = 7.45 * 4
+games = 36 * 0.37
+
+totaal = (7.45 * 4 + 36 * 0.37)
+
+print ("speelhal" +str(totaal))
+
+
